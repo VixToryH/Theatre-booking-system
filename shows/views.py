6c769a2db7cbfc41484e6b2ddac4371946ae4b63
VixToryH bc2025-3 <Viktoriia.Hvozdevych@lnu.edu.ua>
2025-10-20 18:20:08 +0300

    Add DRF setup, serializers, and show endpoints

--- shows/views.py
+++ shows/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Show
+from .serializers import ShowSerializer
 
-# Create your views here.
+# GET /api/shows/ - список усіх вистав/ств. нову виставу для адміна
+class ShowListView(generics.ListCreateAPIView):
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAdminUser()]  # створювати може лише адмін
+        return [permissions.AllowAny()]  # переглядати можуть всі
+
+# GET /api/shows/{id}/ - деталі конкретної вистави/оновити/видалити
+class ShowDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
